70e0031ebf4915ecb132cccf9b91c5316cb7b820
Kim Louar Halos <yumikimmy7@gmail.com>
2026-09-25 09:19:16 +0800

    Fix task routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::resource('tasks', TaskController::class);
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
